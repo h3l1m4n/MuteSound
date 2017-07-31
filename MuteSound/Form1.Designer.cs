d3f15e31e0bd9b6a38221978c56347de6f52c345
alexander <alexander.hellman@outlook.com>
2017-07-31 18:17:06 +0200

    First Upload

--- MuteSound/Form1.Designer.cs
+++ MuteSound/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MuteSound
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,78 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.btTest = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.kluwertseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btTest
+            // 
+            this.btTest.Location = new System.Drawing.Point(26, 12);
+            this.btTest.Name = "btTest";
+            this.btTest.Size = new System.Drawing.Size(75, 23);
+            this.btTest.TabIndex = 0;
+            this.btTest.Text = "Alt + N";
+            this.btTest.UseVisualStyleBackColor = true;
+            this.btTest.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Fuck Airplanes";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kluwertseToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(129, 48);
+            // 
+            // kluwertseToolStripMenuItem
+            // 
+            this.kluwertseToolStripMenuItem.Name = "kluwertseToolStripMenuItem";
+            this.kluwertseToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.kluwertseToolStripMenuItem.Text = "Kluwert.se";
+            this.kluwertseToolStripMenuItem.Click += new System.EventHandler(this.kluwertseToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(143, 41);
+            this.Controls.Add(this.btTest);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "Fuck Airplanes";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btTest;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kluwertseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- MuteSound/Form1.cs
+++ MuteSound/Form1.cs
@@ -1,20 +1,111 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace MuteSound
 {
-    public partial class Form1 : Form
+    public partial class FrmMain : Form
     {
-        public Form1()
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int processId);
+        //All modifier buttons, incase of Configfile in the future.
+        private const int MOD_CONTROL = 0x0002;
+        private const int MOD_SHIFT = 0x0004;
+        private const int WM_HOTKEY = 0x0312;
+        public const int MOD_ALT = 0x0001;
+        private bool decreased = false;
+        public FrmMain()
         {
             InitializeComponent();
+           
+            this.ShowInTaskbar = false;
+            SetHotKeys();
+        }
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MuteGame();
+        }
+        private void MuteGame()
+        {
+            if (VolumeMixer.IsGameMuted())
+            {
+                VolumeMixer.MuteGame(false);
+            }
+            else
+            {
+                VolumeMixer.MuteGame(true);
+            }
+        }
+
+        private void readconfig()
+        {
+            //Read out config
+            //Game name, incase it was to be used for another proccess
+            //Such as Hotkeys, Prefferd volumelevels etc.
+        }
+        private void DecreaseGame()
+        {
+            if (decreased) { VolumeMixer.SetGameVolume(50);
+                decreased = true;
+            }
+         
+            else
+            { 
+                VolumeMixer.SetGameVolume(100);
+                decreased = false;
+            }
+        }
+        private void SetHotKeys()
+        {
+            RegisterHotKey(Handle, 1, MOD_ALT,78);
+            RegisterHotKey(Handle, 2, MOD_ALT, 77);
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY && (int)m.WParam == 1)
+                try
+                {
+                   MuteGame();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("WHAT THE FUCK JUST HAPPEND? \n I CANT MUTE THE FUCKING AIRPLANE", @"Error", MessageBoxButtons.OK);
+                }
+            if (m.Msg == WM_HOTKEY && (int)m.WParam == 2)
+                try
+                {
+
+                    DecreaseGame();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Cant decrease sound", @"Error", MessageBoxButtons.OK);
+                }
+            base.WndProc(ref m);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UnregisterHotKey(Handle, 1);
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void kluwertseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.kluwert.se");
         }
     }
+
+
 }
